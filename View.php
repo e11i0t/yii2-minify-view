9eb9362832c956bf5f06ff3cdfe3e807686fdb49
Elliot <nvalois1+github@gmail.com>
2016-11-26 18:43:42 +0100

    Update View.php

--- View.php
+++ View.php
@@ -1,8 +1,46 @@
 <?php
 /**
  * View.php
- * @author Revin Roman
+ * @author for minify Revin Roman
+ * @author for ClientScriptView MGHollander
  * @link https://rmrevin.ru
+ */
+
+/**
+ * ClientScriptView class is an extension on Yii's View to register JavaScript and CSS inside a view.
+ * It keeps your code hightlighted and readable by your IDE.
+ *
+ * I've modified the code to remove the script ans style tags using a regular expression, because it may occur
+ * that other HTML tags are used within a JavaScript of Style string.
+ *
+ * Usage is simular to {@link View::registerJs} and {@link View::registerCss}
+ * The <script> and <style> tags are required for highlighting and IDE functionallity and will be removed automatically.
+ *
+ * Example:
+ *
+ * <?php $this->beginJs(); ?>
+ * <script>
+ * // Write your JavaScript here.
+ * </script>
+ * <?php $this->endJs(); ?>
+ *
+ * <?php $this->beginJs(View::POS_HEAD, 'unique-id'); ?>
+ * <script type="text/javascript">
+ * // Write your JavaScript here.
+ * </script>
+ * <?php $this->endJs(); ?>
+ *
+ * <?php $this->beginCss(); ?>
+ * <style>
+ * // Write your CSS here.
+ * </style>
+ * <?php $this->endCss(); ?>
+ *
+ * <?php $this->beginCss(['media' => 'screen'], 'unique-id'); ?>
+ * <style type="text/css">
+ * // Write your CSS here.
+ * </style>
+ * <?php $this->endCss(); ?>
  */
 
 namespace rmrevin\yii\minify;
@@ -17,6 +55,32 @@ use yii\web\Response;
  */
 class View extends \yii\web\View
 {
+    //
+    //  ClientScriptView
+    //
+
+    /**
+     * @see {@link View::registerJs} key parameter
+     * @var string
+     */
+    protected $key;
+
+    /**
+     * @see {@link View::registerJs} position parameter
+     * @var integer
+     */
+    protected $position;
+
+    /**
+     * @see {@link View::registerCss} options array
+     * @var array
+     */
+    protected $options;
+
+
+    //
+    //  Minify
+    //
 
     /**
      * @var bool
@@ -183,5 +247,49 @@ class View extends \yii\web\View
                 $this->registerAssetFiles($bundle);
             }
         }
+    }
+
+
+    //
+    //  ClientScriptView
+    //
+
+
+    /**
+     * Set attributes and turn on output buffering.
+     */
+    public function beginJs($position = self::POS_READY, $key = null) {
+        $this->position = $position;
+        $this->key = $key;
+
+        ob_start();
+        ob_implicit_flush(false);
+    }
+
+    /**
+     * Get default position if non set, get current buffer contents, delete current output buffer and register the contents.
+     * @see {@link CClientScript::registerScript} return description
+     */
+    public function endJs() {
+        parent::registerJs(preg_replace('/\s*<\/?script(.*)>\s*/i', '', ob_get_clean()), $this->position, $this->key);
+    }
+
+    /**
+     * Set attributes and turn on output buffering.
+     */
+    public function beginCss($options = [], $key = null) {
+        $this->options = $options;
+        $this->key = $key;
+
+        ob_start();
+        ob_implicit_flush(false);
+    }
+
+    /**
+     * Get default position if non set, get current buffer contents, delete current output buffer and register the contents.
+     * @see {@link CClientScript::registerScript} return description
+     */
+    public function endCss() {
+        parent::registerCss(preg_replace('/\s*<\/?style(.*)>\s*/i', '', ob_get_clean()), $this->options, $this->key);
     }
 }
